5c52e6d8f30b3f5a202c3956ce695bd5bd703d21
Hyeongho Park <harrypark624@gmail.com>
2024-04-29 13:08:13 +0900

    delete button

--- EnvironmentalSimulation/Form1.Designer.cs
+++ EnvironmentalSimulation/Form1.Designer.cs
@@ -37,8 +37,6 @@
             this.방1난방 = new System.Windows.Forms.Button();
             this.방1에어컨 = new System.Windows.Forms.Button();
             this.cld = new System.Windows.Forms.ColorDialog();
-            this.button1 = new System.Windows.Forms.Button();
-            this.방2.SuspendLayout();
             this.방1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,7 +65,6 @@
             // 
             // 방2
             // 
-            this.방2.Controls.Add(this.button1);
             this.방2.Location = new System.Drawing.Point(305, 18);
             this.방2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.방2.Name = "방2";
@@ -137,15 +134,6 @@
             this.방1에어컨.Text = "에어컨";
             this.방1에어컨.UseVisualStyleBackColor = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(140, 130);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -158,7 +146,6 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.방2.ResumeLayout(false);
             this.방1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -175,7 +162,6 @@
         private System.Windows.Forms.Button 방1난방;
         private System.Windows.Forms.Button 방1에어컨;
         private System.Windows.Forms.ColorDialog cld;
-        private System.Windows.Forms.Button button1;
     }
 }
 
